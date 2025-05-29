59c5ba9676941ef7730f0577e0920b79006a5d45
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-05-29 11:23:49 +0100

    Exercicio 05

---
+++ Exercicio05.py
@@ -0,0 +1,4 @@
+#Faca um programa que leia um numero inteiro e mostre na tela o seu sucessor e o seu antecessor
+n = int(input('Digite um numero: '))
+print('O seu sucessor do numero', n, 'e: {}'.format(n + 1))
+print('E o seu antecessor e: {}'.format(n - 1))
