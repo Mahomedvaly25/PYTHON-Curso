302adcce66b9f417792bbc161d2d9d6625501a8c
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-11 01:01:10 +0100

    Exercicio 26

--- EXERCICIOS/Exercicio26.py
+++ EXERCICIOS/Exercicio26.py
@@ -4,7 +4,7 @@
 # Em que posicao ela aparece pela 1a vez?
 # Em que posicao ela aparece pela ultima vez?
 
-frase = str(input('Digite uma Frase: ')).strip().lower()
+frase = str(input('Digite uma Frase qualquer: ')).strip().lower()
 print('A letra "A" aparece {} vezes'.format(frase.count('a')))
 print('A posicao da 1a letra "A" aparecau na posicao {}'.format(frase.find('a')+1))
 print('A letra "A" apareceu pela ultima vez na posicao {}'.format(frase.rfind('a')+1))
