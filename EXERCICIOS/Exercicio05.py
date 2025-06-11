a937d3859c5ea2dee151fdbe152657df1d8f7fbb
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-11 23:44:36 +0100

    Exercicio 05

--- EXERCICIOS/Exercicio05.py
+++ EXERCICIOS/Exercicio05.py
@@ -1,4 +1,4 @@
 #Faca um programa que leia um numero inteiro e mostre na tela o seu sucessor e o seu antecessor
 n = int(input('Digite um numero: '))
-print('O seu sucessor do numero', n, 'e: {}'.format(n + 1))
-print('E o seu antecessor e: {}'.format(n - 1))
+print('O seu sucessor do numero', n, 'e: \033[31m{}\033[m'.format(n + 1))
+print('E o seu antecessor e: \033[35m{}\033[m'.format(n - 1))
