5c3e448a60c90613ed5188ceb2f9d6b8f9c4cf9b
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-14 18:52:05 +0100

    Exercicio 38

---
+++ EXERCICIOS/Exercicio38.py
@@ -0,0 +1,15 @@
+# Escreva um programa que leia dois numeros inteiros e compare-os, mostrando na tela uma menssagem:
+# - O primeiro valor é maior
+# - O segundo valor é maior
+# - Nao existe o valor maior, os dois sao iguais
+
+primeiro = float(input('Digite o 1o Valor: '))
+segundo = float(input('Digite o 2o Valor: '))
+
+if primeiro > segundo:
+    print('O PRIMEIRO valor {} é o MAIOR'.format(primeiro))
+elif segundo > primeiro:
+    print('O SEGUNDO valor {} é o MAIOR'.format(segundo))
+else:
+    print('Nao EXISTE um valor MAIOR, os dois sao IGUAIS.')
+
