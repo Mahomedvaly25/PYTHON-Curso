3280cce63bf410436b689ae68cd070dcbeaf0e4f
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-16 21:07:49 +0100

    Exercicio 47

---
+++ EXERCICIOS/Exercicio47.py
@@ -0,0 +1,12 @@
+#Crie um programa que mostre na tela todos os numeros pares que estao no intervalo entre 1 a 50
+
+
+for cont in range(1,51):
+    if cont % 2 == 0:
+        print(cont, end=' ')
+
+# OU
+# Solucao encurtada do professor:
+
+# for cont in renge(2,500,2):
+#     print( n, end=' ')
