5ffaa99c6c0fb6f50e1be19956b92f33a9b9067a
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-17 20:36:15 +0100

    Exercicio 56

---
+++ EXERCICIOS/Exercicio56.py
@@ -0,0 +1,5 @@
+# Densevolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa mostre:
+# - A media de idade do grupo.
+# - Qual e o nome do homem mais velho
+# - Quantas mulheres tem com menos de 20 anos.
+
