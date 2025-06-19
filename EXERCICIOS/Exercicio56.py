21f40302a1c368bcd57e0573e6d6c535f4c11ed8
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-19 13:13:29 +0100

    Exercicio 56

--- EXERCICIOS/Exercicio56.py
+++ EXERCICIOS/Exercicio56.py
@@ -7,15 +7,22 @@ somaidade = 0
 media = 0
 homemvelho = 0
 nomevelho = ''
+mulhernova = 0
 for c in range(1,4):
     print("----------- {}o PESSOA------------")
     nome = str(input('Digite seu nome completo: ')).strip()
     idade = int(input('Digite sua idade: '))
     sexo = str(input('Digite seu sexo [M/F]: ')).strip().strip()
-
     somaidade = somaidade + idade
-
     if c == 1 and sexo in 'M,m':
-        somaidade = idad
-    media = somaidade / 4
+        homemvelho = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > homemvelho:
+        homemvelho = idade
+        nomevelho = nome
+    if sexo in 'F,f' and idade < 20:
+        mulhernova += 1
+media = somaidade / 4
 print("A media de idade foi {}".format(media))
+print('O Nome do homem mais velho e {} e tem {} Anos.'.format(nomevelho,homemvelho))
+print('Foram {} mulheres novas do grupo.'.format(mulhernova))
