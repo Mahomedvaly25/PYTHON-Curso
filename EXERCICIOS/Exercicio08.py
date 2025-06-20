6168556971e9872384febb96ac6f6f6bdc0c68e3
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-20 22:04:20 +0100

    Exercicio 08

--- EXERCICIOS/Exercicio08.py
+++ EXERCICIOS/Exercicio08.py
@@ -1,9 +1,9 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centimentros a milimetros
 
-print('='*70)
+print('\033[33m='*70,'\033[m')
 Mt = float(input('Digite um valor em metros para ver a conversao em CENTIMETROS E MILIMETROS:  '))
 Cm = Mt * 100
 mm = Mt * 1000
-print('='*70)
+print('\033[33m='*70,'\033[m')
 print('{} mt = {} cm'.format(Mt,Cm))
 print('{} mt = {} mm'.format(Mt,mm))
