43b15c3e89a596231ca0449fe38f7840429b7b23
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-20 21:55:46 +0100

    Exercicio 06

--- EXERCICIOS/Exercicio06.py
+++ EXERCICIOS/Exercicio06.py
@@ -3,4 +3,4 @@ n = int(input('Digite um numero: '))
 d = n * 2
 t = n * 3
 rq = n**(1/2)
-print('O dobro de {} e: {}, o triplo e {}, a raiz quadrada e {:.3}'.format(n,d,t,rq))
+print('O dobro de \033[32m{}\033[m e:\033[31m{}\033[m, o triplo e \033[31m{}\033[m, a raiz quadrada e \033[31m{:.3}\033[m'.format(n,d,t,rq))
