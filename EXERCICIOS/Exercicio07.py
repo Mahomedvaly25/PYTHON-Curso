781d29dde0dabd31c101580479f5d4f8bafea84d
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-20 22:04:01 +0100

    Exercicio 07

--- EXERCICIOS/Exercicio07.py
+++ EXERCICIOS/Exercicio07.py
@@ -2,4 +2,7 @@
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2)**1/2
-print('O Aluno com as notas {} e {} teve uma media final de {}'.format(n1, n2, m))
+if m >= 10:
+    print('O Aluno com as notas \033[32m{}\033[m e \033[32m{}\033[m teve uma media final de \033[32m{}\033[m'.format(n1, n2, m))
+else:
+    print('O Aluno com as notas \033[31m{}\033[m e \033[31m{}\033[m teve uma media final de \033[31m{}\033[m'.format(n1,n2,m))
