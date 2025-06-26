9298ba152f5f0aa3aa6b37e69ecedf0a043d0eb1
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-26 16:41:44 +0100

    Execicio 69

---
+++ EXERCICIOS/Exercicio69.py
@@ -0,0 +1,7 @@
+# Crie um programa que leia a idade e o Sexo de varias pessoas.
+# A cada pessoa cadastrada o programa devera perguntar se o usuario quer ou nao continuar.
+# No final Mostre:
+# - Quantas pessoas tem mais de 18 Anos.
+# - Quantos homens foram cadastrados
+# - Quantas Mulheres com menos de 20 Anos.
+
