755dc21f435de47d110c9f81ed275042805c17c6
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-27 20:41:44 +0100

    Execicio 71

--- EXERCICIOS/Exercicio71.py
+++ EXERCICIOS/Exercicio71.py
@@ -3,3 +3,29 @@
 # No final vai informar quantas cedulas de cada valor serao entregues.
 # Obs: Considere que o caixa possui cedulas de 50.00 Eur, 20.00 Eur, 10.00 Eur e 1.00 Eur
 
+print('='*20)
+print('CAIXA ELECTRONICO')
+print('='*20)
+
+quantidadeNotas = totalnota = 0
+nota = 50
+
+valorlevantar = int(input('Qual valor deseja levantar: '))
+totalnota = valorlevantar
+while True:
+    if totalnota >=  nota:
+        totalnota -= nota
+        quantidadeNotas += 1
+    else:
+        if quantidadeNotas > 0:
+            print(f'Sao {quantidadeNotas} Notas de {nota} Euros.')
+        if nota == 50:
+            nota = 20
+        elif nota == 20:
+            nota = 10
+        elif nota == 10:
+            nota = 1
+        quantidadeNotas = 0
+        if totalnota == 0:
+            break
+print('OBRIGADO')
