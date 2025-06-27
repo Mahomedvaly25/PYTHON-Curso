da3852b60d35ae0eb574dab3192b009b68ee7a04
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-27 18:00:26 +0100

    Execicio 71

---
+++ EXERCICIOS/Exercicio71.py
@@ -0,0 +1,5 @@
+# Crie um programa que simule o funcionamento do caixa electronico.
+# No inicio pergunte ao usuario qual sera o valor a ser sacado (numeros inteiros).
+# No final vai informar quantas cedulas de cada valor serao entregues.
+# Obs: Considere que o caixa possui cedulas de 50.00 Eur, 20.00 Eur, 10.00 Eur e 1.00 Eur
+
