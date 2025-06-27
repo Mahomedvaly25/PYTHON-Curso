78b64d2b9780f933743c69f318aaa1f3be7b39bb
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-27 18:00:09 +0100

    Execicio 69

--- EXERCICIOS/Exercicio69.py
+++ EXERCICIOS/Exercicio69.py
@@ -5,4 +5,24 @@
 # - Quantos homens foram cadastrados
 # - Quantas Mulheres com menos de 20 Anos.
 
-print("CADASTRE UMA PESSOA")
+idade = pessoas = pmais18 = tothomens = mulheres20 = 0
+print(f'{"CADASTRE UMA PESSOA":^40}')
+print('-='*20)
+while True:
+    sexo = resp = ' '
+    idade = int(input('IDADE: '))
+    while sexo not in 'MF':
+        sexo = str(input('SEXO:[M/F] ')).strip().upper()[0]
+    if idade > 18:
+        pmais18 += 1
+    if sexo == 'M':
+        tothomens += 1
+    if sexo == 'M' and idade < 20:
+        mulheres20 += 1
+    pessoas += 1
+    while resp not in 'S/N':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'{pessoas} Pessoas cadastradas e {pmais18} com mais de 18 anos')
+print(f'Dentre eles foram {tothomens} Homens e {mulheres20} mulheres com menos de 20 anos')
