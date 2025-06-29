aece2e4719e8c30d96254d3b7980f9d91e6a9b10
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-29 18:45:03 +0100

    Execicio 73

---
+++ EXERCICIOS/Exercicio73.py
@@ -0,0 +1,5 @@
+# Crie uma tupla preenchida com os 20 primeiro colocados do campeonato Brasileiro de Futebol, na ordem de colocacao . Depois mostre:
+# a) Apenas os 5 primeiros colocados
+# b) Os ultimos 4 colocados da tabela
+# c) Uma lista com os times em ordem alfabetica
+# d) Em que posicao na tabela esta o time Chapecoense
