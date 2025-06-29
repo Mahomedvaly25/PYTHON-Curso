dc2f4cda37e9dcd028ca87209ef25e1f8ab933e2
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-29 18:45:26 +0100

    Execicio 75

---
+++ EXERCICIOS/Exercicio75.py
@@ -0,0 +1,4 @@
+# Desenvolve um programa que leia 4 valores pelo teclado e guarde-os em uma tupla. No final mostre:
+# a) Quantas vezes apareceu o valor 9
+# b) Em que posicao foi digitado o 1o valor 3.
+# c) Quais foram os numeros Pares.
