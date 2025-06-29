20a50aec25e7abf0f771ef6b175d6979a24a43df
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-06-29 18:43:41 +0100

    Execicio 72

---
+++ EXERCICIOS/Exercicio72.py
@@ -0,0 +1,2 @@
+# Crie um programa que tenha uma tupla totalmente preechida com uma contagem por extenso, de 0 ate 20
+# Seu programa devera ler um numero pelo teclado entre 0 a 20 e mostra-lo por extenso.
