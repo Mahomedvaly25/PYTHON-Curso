0f05621922779a9f7559eb79fcae48ddb618b21f
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-04 21:26:55 +0100

    Execicio 73

--- EXERCICIOS/Exercicio73.py
+++ EXERCICIOS/Exercicio73.py
@@ -1,5 +1,13 @@
-# Crie uma tupla preenchida com os 20 primeiro colocados do campeonato Brasileiro de Futebol, na ordem de colocacao . Depois mostre:
+# Crie uma tupla preenchida com os 20 primeiro colocados do ranking da UEFA de Futebol masculino, na ordem de colocacao . Depois mostre:
 # a) Apenas os 5 primeiros colocados
 # b) Os ultimos 4 colocados da tabela
 # c) Uma lista com os times em ordem alfabetica
-# d) Em que posicao na tabela esta o time Chapecoense
+# d) Em que posicao na tabela esta o time Portugal?
+
+equipas = ('inglaterra','Italia','Espanha','Alemanha','Franca','Paises-Baixo','Portugal',
+           'Beilgica','Chequia','Turquia','Noruega','Grecia','Austria','Escocia',
+           'Polonia','Dinamarca','Suica','Israel','Chipre','Suecia')
+print(f'As Cinco primerira colocadas sao: {equipas[:5]}')
+print(f' Os Ultimos Quatro colocados sao: {equipas[-4:]}')
+print(sorted(equipas))
+print(f'O Time de Portugal esta na posicao {equipas.index('Portugal')+1}')
