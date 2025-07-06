b35262003ccf4fb78a1f5041272feac9165e9215
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-06 17:59:54 +0100

    Exercicio 79

---
+++ EXERCICIOS/Exercicio79.py
@@ -0,0 +1,4 @@
+# Crie um programa onde o usuario pode digitar varios valores numericos e cadastre-os em uma lista.
+# Caso o numero ja exista la dentro, ele nao sera adicionado.
+# No Final serao exibidos todos os valores unicos digitados em ordem crescente.
+
