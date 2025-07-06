a6fa5d97400da1d239e452dab08b96387762b704
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-06 18:00:33 +0100

    Exercicio 83

---
+++ EXERCICIOS/Exercicio83.py
@@ -0,0 +1,3 @@
+# Crie um programa onde o usuario digite uma expressao qualquer que use parenteses.
+# Seu aplicativo devera analisar se a expressao passada esta com parenteses aberto e fechados na ordem correta.
+
