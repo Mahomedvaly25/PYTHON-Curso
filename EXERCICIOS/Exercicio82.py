11f309c96d0f76852d3fb1eb047b903f78468b5d
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-06 18:00:24 +0100

    Exercicio 82

---
+++ EXERCICIOS/Exercicio82.py
@@ -0,0 +1,4 @@
+# Crie um programa que vai ler varios numeros e colocar em uma lista.
+# Depois disso, crie 2 listas extras que vao contar apenas os valores pares e os valores impares digitados, respectivamente.
+# Ao final, mostre o conteudo das 3 listas geradas.
+
