9a86ac8cc01b4c3a0ad3cd38df8965a5531de980
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-06 18:00:12 +0100

    Exercicio 81

---
+++ EXERCICIOS/Exercicio81.py
@@ -0,0 +1,4 @@
+# Crie um programa que vai ler varios numeros e colocar em uma lista, depois disso mostre:
+# a) Quantos numeros foram digitados.
+# b) A lista de valores, ordenada em ordem decrescente
+# c) Se o valor 5 foi digitado e esta ou nao na lista.
