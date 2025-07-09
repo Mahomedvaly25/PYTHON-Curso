c4b8cc597904b0a3ce64f6fb438a312c30be3a74
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-09 10:47:50 +0100

    Exercicio 80

--- EXERCICIOS/Exercicio80.py
+++ EXERCICIOS/Exercicio80.py
@@ -2,3 +2,17 @@
 # Valores ja na posicao correta de insercao (sem usar o sort())
 # No Final mostre a lista ordenada na tela.
 
+lista = []
+for c in range(0,5):
+    n = int(input('Digite um Numero: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                break
+            pos += 1
+
+print(lista)
