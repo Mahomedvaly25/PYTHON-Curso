33583d699a927fd87a2948facfa1e96b5ad09c30
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-09 10:47:12 +0100

    Exercicio 79

--- EXERCICIOS/Exercicio79.py
+++ EXERCICIOS/Exercicio79.py
@@ -2,3 +2,21 @@
 # Caso o numero ja exista la dentro, ele nao sera adicionado.
 # No Final serao exibidos todos os valores unicos digitados em ordem crescente.
 
+resp = 'S'
+num = []
+while True:
+    n = int(input('Digite um numero: '))
+    if n not in num:
+        num.append(n)
+        print('Adicionao com sucesso...')
+    else:
+        print('Numero repetido, Tente Novamente: ')
+
+    resp = str(input('Quer continuar: [S/N]')).upper().strip()[0]
+    while resp not in 'SN':
+        print('Resposta ERRADA, Tente Novamente.')
+        resp = str(input('Quer continuar: [S/N]')).upper().strip()[0]
+    if resp == 'N':
+        break
+
+print(f'Digitou os valores: {sorted(num)}')
