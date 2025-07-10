dc2dffaf73d2f7d83116c27db9a42ea69f61c63f
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-10 20:37:01 +0100

    Exercicio 73

--- EXERCICIOS/Exercicio73.py
+++ EXERCICIOS/Exercicio73.py
@@ -7,7 +7,7 @@
 equipas = ('inglaterra','Italia','Espanha','Alemanha','Franca','Paises-Baixo','Portugal',
            'Beilgica','Chequia','Turquia','Noruega','Grecia','Austria','Escocia',
            'Polonia','Dinamarca','Suica','Israel','Chipre','Suecia')
-print(f'As Cinco primerira colocadas sao: {equipas[:5]}')
-print(f' Os Ultimos Quatro colocados sao: {equipas[-4:]}')
-print(sorted(equipas))
+print(f'As Cinco primerira colocadas sao: \033[33m{equipas[:5]}\033[m')
+print(f' Os Ultimos Quatro colocados sao: \033[32m{equipas[-4:]}\033[m')
+print(f'As Equipas ordenadas sao:  \033[34m{sorted(equipas)}\033[m')
 print(f'O Time de Portugal esta na posicao {equipas.index('Portugal')+1}')
