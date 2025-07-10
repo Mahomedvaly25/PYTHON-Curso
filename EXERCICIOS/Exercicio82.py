953c7413fa26daf7fd0234423809212bbdcdf41d
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-10 23:27:49 +0100

    Exercicio 82

--- EXERCICIOS/Exercicio82.py
+++ EXERCICIOS/Exercicio82.py
@@ -1,4 +1,24 @@
 # Crie um programa que vai ler varios numeros e colocar em uma lista.
 # Depois disso, crie 2 listas extras que vao contar apenas os valores pares e os valores impares digitados, respectivamente.
 # Ao final, mostre o conteudo das 3 listas geradas.
+list = []
+listpar = []
+listimpar = []
+while True:
+    list.append(int(input('Digite um numero: ')))
+    resp = str(input('Quer Continuar? [S/N]')).upper().strip()[0]
+    while resp not in 'SN':
+        print('Resposta invalida, Tente Novamente...')
+        resp = str(input('Quer Continuar? [S/N]')).upper().strip()[0]
+    if resp =='N':
+        break
 
+for cont in list:
+    if cont % 2 == 0:
+        listpar.append(cont)
+    else:
+        listimpar.append(cont)
+
+print(list)
+print(listpar)
+print(listimpar)
