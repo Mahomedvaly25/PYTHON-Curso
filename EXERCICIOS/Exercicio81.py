79563bd35a0c0970eb08d544f35dc702df630b01
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-10 23:27:34 +0100

    Exercicio 81

--- EXERCICIOS/Exercicio81.py
+++ EXERCICIOS/Exercicio81.py
@@ -2,3 +2,23 @@
 # a) Quantos numeros foram digitados.
 # b) A lista de valores, ordenada em ordem decrescente
 # c) Se o valor 5 foi digitado e esta ou nao na lista.
+
+quant = 0
+list = []
+while True:
+    list.append(int(input('Digite um numero: ')))
+
+    resp = str(input('Quer Continuar? [S/N]')).upper().strip()[0]
+    while resp not in 'SN':
+        print('Resposta invalida, Tente Novamente.')
+        resp = str(input('Quer Continuar? [S/N]')).upper().strip()[0]
+    if resp == 'N':
+        break
+
+list.sort(reverse=True)
+print(f'Voce digitou {len(list)} elementos.')
+print(f'Os Valores em ordem Decrescente sao: {list}')
+if 5 in list:
+    print('O Valor (5) faz parte da lista.')
+else:
+    print('O Valor (5) nao foi encontrado na lista.')
