1545cc924aa05c905c2765f8adea6dd562cc48b6
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-11 11:29:17 +0100

    Exercicio 77

--- EXERCICIOS/Exercicio77.py
+++ EXERCICIOS/Exercicio77.py
@@ -4,9 +4,9 @@
 palavras = ('Aprender','Programar','Linguagem','Python','Curso','Gratis','Estudar','praticar','trabalhar','mercado','trabalhador','futuro')
 print('-'*50)
 for n in palavras:
-    print(f'\nNa palavra {n.upper()} temos:{' ':<2}',end='')
+    print(f'\nNa palavra \033[33m{n.upper()}\033[m temos:{' ':<2}',end='')
     for letra in n:
         if letra in 'aeiou':
-            print(f'{letra.lower()}', end=' ')
+            print(f'\033[35m{letra.lower()}\033[m', end=' ')
 print('\n')
 print('-'*50)
