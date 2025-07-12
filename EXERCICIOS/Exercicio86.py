a5f3e6bc173508fae4b9f9fac7eb1e1d9a296eea
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-12 21:27:37 +0100

    Exercicio 86

---
+++ EXERCICIOS/Exercicio86.py
@@ -0,0 +1,7 @@
+# Crie um programa que crie uma matriz de dimensao 3 x 3 e preencha os valores lidos pelo teclado.
+#        |        |
+#   ----------------------
+#        |        |
+#   ----------------------
+#        |        |
+# No final mostre a Matriz na tela, Com a formatacao correta.
