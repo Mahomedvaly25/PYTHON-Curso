765ce729ad58cf76665492264c72237a83a54d64
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-12 21:27:49 +0100

    Exercicio 88

---
+++ EXERCICIOS/Exercicio88.py
@@ -0,0 +1,3 @@
+# Faca um programa que ajude jogadores da mega sena a criar palpites.
+# O programa vai perguntar quantos jogos serao gerados e vai gerar 6 numeros entre 1 a 60 para cada jogo, cadastrando tudo em uma lista composta.
+
