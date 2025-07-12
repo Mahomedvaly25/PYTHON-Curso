c963beffa59667b3a709c2df77885edbe49e3fdd
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-12 21:27:15 +0100

    Exercicio 84

---
+++ EXERCICIOS/Exercicio84.py
@@ -0,0 +1,4 @@
+# Faca um programa que leia nome e peso de varias pessoas, guardando tudo em uma lista. No final mostre:
+# - Quantas pessoas foram cadastradas;
+# - Uma listagem com as pessoas mais pesadas; (100kg)
+# - Uma listagem com as pessoas menos pesadas.(70kg)
