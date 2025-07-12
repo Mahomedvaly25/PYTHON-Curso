44b5300614eec3ce8fb1e4ba2b5f905b1a3f4a7a
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-12 21:27:01 +0100

    Exercicio 83

--- EXERCICIOS/Exercicio83.py
+++ EXERCICIOS/Exercicio83.py
@@ -1,3 +1,20 @@
 # Crie um programa onde o usuario digite uma expressao qualquer que use parenteses.
 # Seu aplicativo devera analisar se a expressao passada esta com parenteses aberto e fechados na ordem correta.
 
+simbolos = []
+expr = str(input('Digite uma EXPRESSAO Matematica qualquer: '))
+for cont in expr:
+    if cont == '(':
+        simbolos.append('(')
+
+    elif cont == ')':
+        if len(simbolos) > 0:
+            simbolos.pop()
+        else:
+            simbolos.append(')')
+            break
+
+if len(simbolos) == 0:
+    print('A Expressao inserida esta CORRETA.')
+else:
+    print('A Expressao inserida esta ERRADA.')
