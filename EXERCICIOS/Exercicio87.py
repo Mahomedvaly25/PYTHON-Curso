ae7809f906d453815d75328e91651476b9a2da51
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-12 21:27:58 +0100

    Exercicio 87

---
+++ EXERCICIOS/Exercicio87.py
@@ -0,0 +1,4 @@
+# Aprimore o Exercicio anterior (Exerc86), mostrando no final:
+# - A Soma de todos os valores pares digitados.
+# - A Soma dos valores da terceira coluna.
+# - O Maior valor da segunda linha.
