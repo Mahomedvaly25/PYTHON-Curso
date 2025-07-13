adbe83a58a7271909f69e7216b76e2b2855285f6
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-13 18:41:47 +0100

    Aula 17.02

--- Exp Aulas/Aulas17.02.py
+++ Exp Aulas/Aulas17.02.py
@@ -1,7 +1,16 @@
 # Usando o [:] para copiar os valores da variavel a para variavel b, sem criar uma ligacao entre elas
+from random import randint
+#a = [3,5,4,8,9]
+#b = a[:]
+#b[2] = 6
+#print(f'A Lista A: {a}')
+#print(f'A Lista B: {b}')
+#print(a[len(a)])
 
-a = [3,5,4,8,9]
-b = a[:]
-b[2] = 6
-print(f'A Lista A: {a}')
-print(f'A Lista B: {b}')
+
+lista = []
+for n in range(0,5):
+    lista.append(randint(2,40))
+print(lista)
+print(lista[len(lista)-1])
+print(len(lista))
