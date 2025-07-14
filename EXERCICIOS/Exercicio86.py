caefcbcff974db5b13d85c177ef9a528649b3456
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-14 22:34:09 +0100

    Exercicio86

--- EXERCICIOS/Exercicio86.py
+++ EXERCICIOS/Exercicio86.py
@@ -5,3 +5,14 @@
 #   ----------------------
 #        |        |
 # No final mostre a Matriz na tela, Com a formatacao correta.
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input(f'Digite um Numero na posicao [{l},{c}]: '))
+print('-=' * 30)
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]',end='')
+    print()
+print(pares)
