82956e90b1c21467d3c605bf99c76ef46a9365e7
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-07-14 22:34:32 +0100

    Exercicio 88

--- EXERCICIOS/Exercicio88.py
+++ EXERCICIOS/Exercicio88.py
@@ -1,3 +1,8 @@
 # Faca um programa que ajude jogadores da mega sena a criar palpites.
 # O programa vai perguntar quantos jogos serao gerados e vai gerar 6 numeros entre 1 a 60 para cada jogo, cadastrando tudo em uma lista composta.
 
+vezes = 0
+from random import randint
+vezes = int(input('Quantos jogos para sortear? '))
+for choice in range(1,(vezes):
+    num = randint(1,60)
