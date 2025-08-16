bd994343f1fd02e50acf16452a56861bfe41381e
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-16 20:52:27 +0100

    Exercicio 86.01

---
+++ EXERCICIOS/Exercicio86.01.py
@@ -0,0 +1,18 @@
+# Crie um programa que crie uma matriz de dimensao 3 x 3 e preencha os valores lidos pelo teclado.
+#        |        |
+#   ----------------------
+#        |        |
+#   ----------------------
+#        |        |
+# No final mostre a Matriz na tela, Com a formatacao correta.
+
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+
+for l in range(0,3):
+    for c in range(0,3):
+        matriz [l][c] = int(input(f'Digite um numero na posicao [{l},{c}]: '))
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]',end='')
+    print()
