af17edf0575197ffb8cec716669699a6d0100e3c
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-16 20:52:39 +0100

    Exercicio 88.01

--- EXERCICIOS/Exercicio88.01.py
+++ EXERCICIOS/Exercicio88.01.py
@@ -1,3 +1,29 @@
 '''Faca um programa que ajude jogadores da mega sena a criar palpites.
 O programa vai perguntar quantos jogos serao gerados e vai gerar 6 numeros entre 1 a 60 para cada jogo, cadastrando tudo em uma lista composta.'''
 
+from random import randint
+
+lista = []
+listaprincipal = []
+cont = totalrepeticoes = 0
+
+repeticoes = int(input('Quantos sorteios pretende sortear? : '))
+
+while totalrepeticoes < repeticoes:
+    cont = 0
+
+    while True:
+        num = randint(1,60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    listaprincipal.append(lista[:])
+    lista.clear()
+    totalrepeticoes += 1
+
+for i, l in enumerate(listaprincipal):
+    print(f'JOGO {i+1}: {l}')
+
