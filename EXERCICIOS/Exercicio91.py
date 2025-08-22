fcbc194948d46ec8c9ee3a6cc11bcb627af139f3
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-22 18:39:06 +0100

    Exercicio 91

---
+++ EXERCICIOS/Exercicio91.py
@@ -0,0 +1,5 @@
+''' Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatorios.
+Guarde esses resultados em um dicionario.
+No Final coloque esse dicionario em ordem, sabendo que o vencedor tirou o maior numero no dado.'''
+
+
