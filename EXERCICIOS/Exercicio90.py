0239ef0ab6c154c46af20dbfed1c9c6ba649d08c
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-22 21:49:56 +0100

    Exercicio 90

--- EXERCICIOS/Exercicio90.py
+++ EXERCICIOS/Exercicio90.py
@@ -1,3 +1,14 @@
-''' Faca um programa que leia a media de um alun, guardando tambem a situacao em um dicionario.
+''' Faca um programa que leia a media de um aluno, guardando tambem a situacao em um dicionario.
 No final mostre o conteudo da estrutura na tela.'''
 
+aluno = dict()
+aluno['Nome'] = str(input('Nome: '))
+aluno['Media'] = float(input('Media: '))
+situacao = ' '
+if aluno['Media'] >= 7:
+    situacao = 'AProvado'
+if aluno['Media'] < 5:
+    situacao = 'Reprovado'
+if aluno['Media'] > 5 and aluno['Media'] < 7:
+    situacao = 'Recuperacao'
+print(f'{aluno},`{situacao}')
