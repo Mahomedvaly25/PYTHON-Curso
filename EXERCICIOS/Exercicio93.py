28bf81e7376d395c364648d902f1e9e23dc4fd27
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-22 18:39:29 +0100

    Exercicio 93

---
+++ EXERCICIOS/Exercicio93.py
@@ -0,0 +1,5 @@
+''' Crie um programa que gere o aproveitamento de um jogador de futebol.
+O programa vai ler o nome do jogador e quantas partidas ele jogou, depois vai ler a quantidade de golos feito em cada partida.
+No final, tudo isso ira ser guardado em um dicionario, incluindo a quantidade de gols feito durante o campeonato.
+'''
+
