4374f1ab3843e0c99640fc1f94bbb6f8cf356034
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-22 18:39:37 +0100

    Exercicio 94

---
+++ EXERCICIOS/Exercicio94.py
@@ -0,0 +1,8 @@
+''' Crie um programa que leia nome, sexo e idade de varias pessoas, guardando os dados de cada pessoa em um dicionario e todos dicionarios
+em uma lista. No final mostre:
+a) Quantas pessoas foram cadastradas.
+b) A Media de idade do grupo.
+c) Uma lista com todas as mulheres.
+d) Uma lista com todas as pessoas com idade acima da media.
+'''
+
