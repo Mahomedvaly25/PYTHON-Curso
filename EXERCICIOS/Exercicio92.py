5291b5546e0d01dc79c2e9ccaf61485af06bcf28
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-22 18:39:19 +0100

    Exercicio 92

---
+++ EXERCICIOS/Exercicio92.py
@@ -0,0 +1,5 @@
+''' Crie um programa que leia nomes, ano de nascimento e carteira de trabalha e cadastre-os (com idade) em um dicionario se por acaso,
+a CTPS (CARTEIRA DE TRABALHO DE PROVIDENCIA SOCIAL) for diferente de Zero, o dicionario recebera tambem o ano de contratacao e o Salario.
+- Calcule e acrescente para alem da idade, com quantos anos a pessoa vai se aposentar.
+'''
+
