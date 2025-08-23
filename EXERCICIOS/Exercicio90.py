6b5a58d346c4e87a8a1afdd9a2344fea6799a759
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-23 17:52:26 +0100

    Exercicio 90

--- EXERCICIOS/Exercicio90.py
+++ EXERCICIOS/Exercicio90.py
@@ -3,12 +3,14 @@ No final mostre o conteudo da estrutura na tela.'''
 
 aluno = dict()
 aluno['Nome'] = str(input('Nome: '))
-aluno['Media'] = float(input('Media: '))
-situacao = ' '
+aluno['Media'] = float(input(f'Qual media do {aluno["Nome"]}?  '))
+
 if aluno['Media'] >= 7:
-    situacao = 'AProvado'
-if aluno['Media'] < 5:
-    situacao = 'Reprovado'
-if aluno['Media'] > 5 and aluno['Media'] < 7:
-    situacao = 'Recuperacao'
-print(f'{aluno},`{situacao}')
+    aluno['Situacao'] = 'Aprovado'
+elif 5 <= aluno['Media'] < 7:
+    aluno['Situacao'] = 'Recuperacao'
+else:
+    aluno['Situacao'] = 'Reprovado'
+print('-=' * 20)
+for k,v in aluno.items():
+    print(f'{k} = {v}')
