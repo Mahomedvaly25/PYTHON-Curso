a6fa7ae81a1c3f910b2f37a1e44ee1085baa1964
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-24 18:47:33 +0100

    Exercicio 92

--- EXERCICIOS/Exercicio92.py
+++ EXERCICIOS/Exercicio92.py
@@ -1,5 +1,20 @@
-''' Crie um programa que leia nomes, ano de nascimento e carteira de trabalha e cadastre-os (com idade) em um dicionario se por acaso,
+''' Crie um programa que leia nomes, ano de nascimento e carteira de trabalha, cadastre-os (com idade) em um dicionario se por acaso,
 a CTPS (CARTEIRA DE TRABALHO DE PROVIDENCIA SOCIAL) for diferente de Zero, o dicionario recebera tambem o ano de contratacao e o Salario.
 - Calcule e acrescente para alem da idade, com quantos anos a pessoa vai se aposentar.
 '''
-
+from datetime import date
+cadastro = dict()
+anoactual = date.today().year
+cadastro['nome'] = str(input('Digite o nome:  '))
+cadastro['Nascimento'] = int(input(f'Ano de nascimento do {cadastro["nome"]}:  '))
+cadastro['Idade'] = anoactual - cadastro['Nascimento']
+print(cadastro)
+if cadastro['Idade'] >= 18:
+    cadastro['CTrabalho'] = int(input('Indique o No. da Carteira de Trabalho: '))
+    cadastro['AnoContratacao'] = int(input('Indique o ano do inicio do Contrato de trabalho?  '))
+    cadastro['Salario'] = float(input('Indique o Salario:  '))
+    print(cadastro['CTrabalho'])
+else:
+    print('Nao tem idade suficiente para trabalho')
+print(cadastro)
+print('Obrigado')
