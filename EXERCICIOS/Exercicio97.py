76108163a36a1bd47b12adca43ee73f99b6f5683
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-29 21:53:26 +0100

    Exercicio 97

---
+++ EXERCICIOS/Exercicio97.py
@@ -0,0 +1,10 @@
+'''Faca um programa que tenha uma funcao chamada escreva(), que receba um texto qualquer como parametro e mostre uma
+mensagem com tamanho adaptavel.
+Ex:
+escreva('Ola, Mundo!')
+
+Saida:
+~~~~~~~~~~~~~~~
+  Ola, Mundo!
+~~~~~~~~~~~~~~~
+'''
