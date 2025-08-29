22919f5e43dbe228d1bd8c9221f508dcce80fa6e
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-29 21:54:02 +0100

    Exercicio 100

---
+++ EXERCICIOS/Exercicio100.py
@@ -0,0 +1,3 @@
+'''Faca um programa que tenha uma lista chamada numeros e duas funcoes chamadas sorteia() e somaPar(). A Primeira Funcao
+vai sortear 20 numeros e vai coloca-los dentro da lista e a segunda funcao vai mostra a soma entre todos os valores PARES
+sorteados pela funcao anterior.'''
