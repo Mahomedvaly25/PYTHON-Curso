9234ae06f9d531fb9ad3f086948d5d64a070d6d9
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-30 18:01:26 +0100

    Aula 20

---
+++ Exp Aulas/Aula20.py
@@ -0,0 +1,10 @@
+def titulo(t):
+    print('-=' * len(t))
+    print(t)
+    print('-=' * len(t))
+
+
+Nome = str(input('Digite o Nome:  '))
+Aula = str(input('Curso:  '))
+titulo(Nome)
+titulo(Aula)
