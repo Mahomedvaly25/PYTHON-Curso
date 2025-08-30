ef393a0927bc5de54b7168d72f3d0edeafaac9c8
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-30 18:01:13 +0100

    Aula 20.01

---
+++ Exp Aulas/Aula20.01.py
@@ -0,0 +1,8 @@
+def soma(a,b):
+    resl = a + b
+    print(f'A soma entre {a} + {b} = {resl}')
+
+a = int(input('Digite o valor de a):  '))
+b = int(input('Digite o valor de b):  '))
+print('=' * 25)
+soma(a,b)
