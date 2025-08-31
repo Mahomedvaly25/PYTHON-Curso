1620be373777e5f765ad761ec0c44c4b34d821ba
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-08-31 21:56:31 +0100

    Exercicio 96

--- EXERCICIOS/Exercicio96.py
+++ EXERCICIOS/Exercicio96.py
@@ -1,9 +1,9 @@
 '''Faca um programa que tenha uma funcao chamada àrea(). Que receba as dimensoes de um terreno retangular(largura e
 comprimento) e mostre a àrea do terreno.
 '''
-def area(larg, comp):
-    a = larg * comp
-    print(f'A area de um terreno com {larg} x {comp} e de {a} m2.')
+def area(l, c):
+    a = l * c
+    print(f'A area de um terreno com {l} x {c} e de {a} m2.')
 
 print('-=' * 14)
 print('    CONTROL DE TERRENOS')
