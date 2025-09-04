cea182915df2767cb01de727f6fb6e615bf24b2e
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-04 21:58:30 +0100

    Exercicio 104

---
+++ EXERCICIOS/Exercicio104.py
@@ -0,0 +1,6 @@
+'''
+Crie uma programa que tenha a funcao leiaInt(), que  vai funcionar de forma semelhante a funcao input() do python,
+so que fazendo a validacao para aceitar apenas um valor numerico.
+Ex:
+n = leiaInt('Digite um numero:')
+'''
