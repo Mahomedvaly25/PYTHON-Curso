2c2f5f6de16a0e71d92da589585d569bb8de5e16
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-04 21:57:55 +0100

    Exercicio 101

---
+++ EXERCICIOS/Exercicio101.py
@@ -0,0 +1,4 @@
+'''
+Crie um programa que tenha uma funcao chamada voto() que vai receber como parametro o ano de nascimento de uma pessoa,
+retornando o valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO  nas eleicoes.
+'''
