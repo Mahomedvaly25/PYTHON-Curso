ac8ee1221285cf6c3654e18323cd758fca29ade2
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-04 21:58:20 +0100

    Exercicio 103

---
+++ EXERCICIOS/Exercicio103.py
@@ -0,0 +1,5 @@
+'''
+Faca um programa que tenha uma funcao chamada ficha(), que receba 2 parametros opcionais: O nome de um jogador,
+e quantos gols ele marcou.
+O programa devera ser capaz de mostrar a ficha do jogador, mesmo que algum dado nao tenha sido informado corretamente.
+'''
