f2c81b0a3c4db7555030b20d82fa13f98f83f92f
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-09 09:34:06 +0100

    Exercicio 102.01

---
+++ EXERCICIOS/Exercicio102.01.py
@@ -0,0 +1,26 @@
+'''
+Crie um programa que tenha uma funcao factorial() que receba 2 parametros:
+O primeiro que indique o numero a calcular e o outro chamado show, que sera um valor logico(opcional), indicando
+se sera mostrado ou nao na tela o processo de calculo do factorial.
+'''
+
+def factorial(n, show=True):
+    """
+    -> Calcule o Factorial de um Numero.
+    :param n: O Numero a ser calculado.
+    :param show: Para ver o Calculo.
+    :return: Resultado do factorial.
+    """
+    f = 1
+    for c in range(n, 0 , -1):
+        if show:
+            print(f'{c}', end='')
+            if c > 1 :
+                print(f' x ', end='')
+            else:
+                print(f' = ', end='')
+        f *= c
+    return f
+
+print(factorial(5, show=True))
+help(factorial)
