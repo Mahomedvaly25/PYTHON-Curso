6965babcefebf5cee85eef9017bdc92a15409cfa
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-20 09:34:43 +0100

    Exercicio 104

--- EXERCICIOS/Exercicio104.py
+++ EXERCICIOS/Exercicio104.py
@@ -4,3 +4,20 @@ so que fazendo a validacao para aceitar apenas um valor numerico.
 Ex:
 n = leiaInt('Digite um numero:')
 '''
+
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print(f'\033[0;31mERRO: Digite um numero inteiro valido.\033[m')
+        if ok:
+            break
+    return valor
+
+n = leiaInt('Digite um numero: ')
+print(f'O Numero que acabou de digitar e o numero {n}')
