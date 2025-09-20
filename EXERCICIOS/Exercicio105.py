ccb773c24c2ec22c5d7b1f888b89c2c77fa0ca34
Mahomedvaly25 <mahomedvaly@gmail.com>
2025-09-20 09:35:21 +0100

    Exercicio 105

--- EXERCICIOS/Exercicio105.py
+++ EXERCICIOS/Exercicio105.py
@@ -1,13 +1,33 @@
-def voto(ano):
-    from datetime import date
-    actual = date.today().year
-    idade = actual - ano
-    if idade <= 16 < 18 or idade > 65:
-        print(f'Idade de {idade} = Voto Opcional')
-    elif idade < 16:
-        print(f'Idade de {idade} = Nao tem idade para Voto')
-    else:
-        print(f'Idade de {idade} = Voto Obrigatorio.')
+'''Faca um programa que tenha a funcao notas() que pode receber varias notas de alunos e vai retornar um dicionario com as seguintes informacoes:
+- Quantidade de notas
+- A Maior nota
+- A Menor Nota
+- A Media da turma
+- A Situacao(Opcional)
+- Adicione tambem as docstrings.'''
 
-nasc = int(input('Ano de Nascimento: '))
-print(voto(nasc))
+def notas(*n, sit=False):
+    """
+    -> Funcao para analisar notas e sutuacao de varios alunos.
+    :param n: Uma ou mais notas dos alunos (aceita Varias).
+    :param sit: Valor Opcional, que indica se deve ou nao mostrar a Situacao.
+    :return: Dicionario com varios informacoes sobre a situacao da turma.
+    """
+    dic = {}
+    dic['N. de Nota'] = len(n)
+    dic['Maior Nota'] = max(n)
+    dic['Menor Nota'] = min(n)
+    dic['Media'] = sum(n)/len(n)
+    if sit:
+        if dic['Media'] >= 7:
+            dic['Situacao'] = 'BOA'
+        elif dic['Media'] >= 5:
+            dic['Situacao'] = 'RAZOAVEL'
+        else:
+            dic['Situacao'] = 'RUIM'
+
+    return dic
+
+resp = notas(8,10,4,9, sit=True)
+print(resp)
+help(notas)
